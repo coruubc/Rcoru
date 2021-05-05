592d3d6c19efc81665804b74d3404fbea7000534
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2021-05-05 20:32:08 -0300

    Updated metadata

--- R/read_distribution.R
+++ R/read_distribution.R
@@ -1,16 +1,22 @@
 #' Reads SAU distribution of taxons
 #'
-#' This function loads the distribution of a taxon from DROBO. You have to be connected to the UBC network
-#' and have access to the CORU-DROBO in order to use the function. You can choose to return the data or just a plot for visualization Note that no user/id will be
-#' required but the path wont be found. Note that this function needs/lodas the CORU coordinate grid
+#' This function loads the SAU distribution of a taxon from DROBO. You have to be connected to the UBC network
+#' and have access to the CORU-DROBO in order to use the function. You can choose to return the data or just a map
+#' for visualization.
+#' Note that no user/id will be required but the path wont be found.
+#' Note that this function needs the CORU coordinate grid
 #'
-#'
+#' @author Juliano Palacios Abrantes | j.palacios@oceans.ubc.ca
 #' @param taxon_key is the species to load as taxon key number.
 #' @param box Expects a vector with four values in the following order: low lat, high lat, low long and high long to load geographical specific data.. if FALSE it will load global database
 #' @param root_path Expects the root computer path before DROBO. Note: do not include /
 #' @examples
+#' Note needs coordinate system for running
+#' output data
 #' read_distribution(600004,output = "data",root_path = "/Volumes",coords = Lon_Lat_DBEM)
-#' @return A data frame or a map showing the distribution of the taxa
+#' #' output plot
+#' read_distribution(600004,output = "plot", root_path = "/Volumes",coords = Lon_Lat_DBEM)
+#' @return A data frame or a map showing the distribution of a specific taxa according to the Sea Around Us
 #'
 #'#' @export
 read_distribution <- function(taxon_key, root_path = NA, output = "plot", coords = FALSE){
